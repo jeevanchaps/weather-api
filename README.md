00a63380abbe3023aa9837a40a849cd7387b5003
Jeevan Chapagainn <manutd.jeevan@gmail.com>
2018-01-05 10:08:05 +0545

    README added

---
+++ README.md
@@ -0,0 +1,17 @@
+#Weather API App
+
+## Clone the project
+	```
+	cd <project-name>
+	npm install
+	```
+## Open [WeatherMap] (https://openweathermap.org) signup and generate your own API key. Place it under
+	```
+	const apiKey = '****your-api-key***';
+	```
+## Run the following command
+	```
+	npm start
+	```
+
+Visit the browser at localhost:3000.
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 var request = require('request');
 
-const apiKey = '****your-key***';
+const apiKey = '****your-api-key***';
 
 
 /* GET home page. */
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/',function(req,res,next){
 	let city = req.body.city;
-	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 request(url,function(err,response,body){
 	console.log(body);
@@ -24,7 +24,7 @@ request(url,function(err,response,body){
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weatherText = `It's ${weather.main.temp} degrees Celsius in ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null});
       }
 	}
